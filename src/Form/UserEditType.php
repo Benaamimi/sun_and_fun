c5079df7423e6912679919d57b2efceb791d2b78
Tarik Benaamimi <benaamimit@gmail.com>
2023-11-25 22:50:49 +0100

    mise en place crud user

---
+++ src/Form/UserEditType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class UserEditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('civilite', ChoiceType::class, [
+            'label' => 'Civilité',
+            'choices'  => [
+                'M.' => 'M.',
+                'Mme.' => 'Mme.',
+            ],
+        ])
+        ->add('prenom', TextType::class, [
+            'label' => 'Prénom',
+            'attr' => [
+                'placeholder' => 'Tapez votre prénom'
+            ],
+            'required' => false,
+        ])
+        ->add('nom', TextType::class, [
+            'label' => 'Nom',
+            'attr' => [
+                'placeholder' => 'Tapez votre nom'
+            ],
+            'required' => false,
+        ])
+        ->add('roles', ChoiceType::class, [
+            'choices' => [
+                'Admin' => 'ROLE_ADMIN',
+                'Membre' => 'ROLE_USER'
+            ],
+            'expanded' => true,
+            'multiple' => true,
+        ])
+        ->add('email', EmailType::class, [
+            'label' => 'Adresse email',
+            'attr' => [
+                'placeholder' => 'Tapez votre adresse email'
+            ],
+            'required' => false,
+        ])
+    ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
